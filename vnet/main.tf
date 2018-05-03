8a18c116a598a97a0017c447ca4fc65866f54494
Nick Fosdick <nick@lark-it.com>
2018-05-03 17:19:17 -0600

    Adding resource group

--- vnet/main.tf
+++ vnet/main.tf
@@ -42,3 +42,28 @@ resource "azurerm_public_ip" "public_ip" {
         environment = "Terraform Demo"
     }
 }
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "ssh" {
+    name                = "myNetworkSecurityGroup"
+    location            = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+
