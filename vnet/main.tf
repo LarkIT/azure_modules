45b0ccb737b9eb7984c8ec7924594fa2f0ee3584
Nick Fosdick <nick@lark-it.com>
2018-05-03 17:10:28 -0600

    adding public IP

--- vnet/main.tf
+++ vnet/main.tf
@@ -31,3 +31,14 @@ resource "azurerm_subnet" "subnet" {
     virtual_network_name = "${azurerm_virtual_network.vnet.name}"
     address_prefix       = "10.10.0.0/24"
 }
+
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "${var.location}"
+    resource_group_name          = "${azurerm_resource_group.rg.name}"
+    public_ip_address_allocation = "dynamic"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
