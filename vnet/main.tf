be3f1086fbd46785246b5c38628dcbd6f369e007
Nick Fosdick <nick@lark-it.com>
2018-05-03 17:05:06 -0600

    Adding subnet

--- vnet/main.tf
+++ vnet/main.tf
@@ -11,16 +11,23 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 
-resource "azurerm_route_table" "table" {
-  name                = "${var.environment}_${var.application_name}_routetable"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-}
+#resource "azurerm_route_table" "table" {
+#  name                = "${var.environment}_${var.application_name}_routetable"
+#  location            = "${var.location}"
+#  resource_group_name = "${azurerm_resource_group.rg.name}"
+#}
 
-resource "azurerm_route" "route" {
-  name                = "${var.environment}_${var.application_name}_route"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  route_table_name    = "${azurerm_route_table.table.name}"
-  address_prefix      = "10.10.0.0/16"
-  next_hop_type       = "vnetlocal"
+#resource "azurerm_route" "route" {
+#  name                = "${var.environment}_${var.application_name}_route"
+#  resource_group_name = "${azurerm_resource_group.rg.name}"
+#  route_table_name    = "${azurerm_route_table.table.name}"
+#  address_prefix      = "10.10.0.0/16"
+#  next_hop_type       = "vnetlocal"
+#}
+
+resource "azurerm_subnet" "subnett" {
+    name                 = ""${var.environment}_${var.application_name}_subnet"
+    resource_group_name  = "${azurerm_resource_group.rg.name}"
+    virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+    address_prefix       = "10.10.0.0/24"
 }
