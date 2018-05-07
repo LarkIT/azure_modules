b59848261d9befab58ed740da4a18ad4b5e79931
Nick Fosdick <nick@lark-it.com>
2018-05-06 21:41:24 -0600

    [THEM-5] Clean up and resource_group update

--- vnet/main.tf
+++ vnet/main.tf
@@ -3,45 +3,34 @@
 #  location = "${var.location}"
 #}
 
+locals {
+  resource_group = "test_themis_vnet_rg"
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.environment}_${var.application_name}_vnet"
   address_space       = ["${var.address_space}"]
   location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  resource_group_name = "${local.resource_group}"
 }
-
-
-#resource "azurerm_route_table" "table" {
-#  name                = "${var.environment}_${var.application_name}_routetable"
-#  location            = "${var.location}"
-#  resource_group_name = "${azurerm_resource_group.rg.name}"
-#}
-
-#resource "azurerm_route" "route" {
-#  name                = "${var.environment}_${var.application_name}_route"
-#  resource_group_name = "${azurerm_resource_group.rg.name}"
-#  route_table_name    = "${azurerm_route_table.table.name}"
-#  address_prefix      = "10.10.0.0/16"
-#  next_hop_type       = "vnetlocal"
-#}
 
 resource "azurerm_subnet" "test_dmz_subnet" {
     name                 = "${var.environment}_${var.application_name}_dmz_subnet"
-    resource_group_name  = "${azurerm_resource_group.rg.name}"
+    resource_group_name  = "${local.resource_group}"
     virtual_network_name = "${azurerm_virtual_network.vnet.name}"
     address_prefix       = "${var.network[ "test_dmz_subnet" ]}"
 }
 
 resource "azurerm_subnet" "test_app_subnet" {
     name                 = "${var.environment}_${var.application_name}_app_subnet"
-    resource_group_name  = "${azurerm_resource_group.rg.name}"
+    resource_group_name  = "${local.resource_group}"
     virtual_network_name = "${azurerm_virtual_network.vnet.name}"
     address_prefix       = "${var.network[ "test_app_subnet" ]}"
 }
 
 resource "azurerm_subnet" "test_db_subnet" {
     name                 = "${var.environment}_${var.application_name}_db_subnet"
-    resource_group_name  = "${azurerm_resource_group.rg.name}"
+    resource_group_name  = "${local.resource_group}"
     virtual_network_name = "${azurerm_virtual_network.vnet.name}"
     address_prefix       = "${var.network[ "test_db_subnet" ]}"
 }
@@ -50,7 +39,7 @@ resource "azurerm_subnet" "test_db_subnet" {
 #resource "azurerm_network_security_group" "ssh" {
 #    name                = "myNetworkSecurityGroup"
 #    location            = "${var.location}"
-#    resource_group_name = "${azurerm_resource_group.rg.name}"
+#    resource_group_name = "${local.resource_group}"
 #
 #    security_rule {
 #        name                       = "SSH"
