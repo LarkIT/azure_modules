97f7eca28578890c6e5e0559db7d0591fe933071
Nick Fosdick <nick@lark-it.com>
2018-05-06 22:16:47 -0600

    [THEM-5] - Adding prod subnets

--- vnet/main.tf
+++ vnet/main.tf
@@ -34,3 +34,24 @@ resource "azurerm_subnet" "test_db_subnet" {
     virtual_network_name = "${azurerm_virtual_network.vnet.name}"
     address_prefix       = "${var.network[ "test_db" ]}"
 }
+
+resource "azurerm_subnet" "prod_dmz_subnet" {
+    name                 = "prod_${var.environment}_${var.application_name}_dmz_subnet"
+    resource_group_name  = "${local.resource_group}"
+    virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+    address_prefix       = "${var.network[ "prod_dmz" ]}"
+}
+
+resource "azurerm_subnet" "prod_app_subnet" {
+    name                 = "prod_${var.environment}_${var.application_name}_app_subnet"
+    resource_group_name  = "${local.resource_group}"
+    virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+    address_prefix       = "${var.network[ "prod_app" ]}"
+}
+
+resource "azurerm_subnet" "prod_db_subnet" {
+    name                 = "prod_${var.environment}_${var.application_name}_db_subnet"
+    resource_group_name  = "${local.resource_group}"
+    virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+    address_prefix       = "${var.network[ "prod_db" ]}"
+}
--- vnet/outputs.tf
+++ vnet/outputs.tf
@@ -17,3 +17,15 @@ output "test_app_subnet_id" {
 output "test_db_subnet_id" {
   value = "${azurerm_subnet.test_db_subnet.id}"
 }
+
+output "prod_dmz_subnet_id" {
+  value = "${azurerm_subnet.prod_dmz_subnet.id}"
+}
+
+output "prod_app_subnet_id" {
+  value = "${azurerm_subnet.prod_app_subnet.id}"
+}
+
+output "prod_db_subnet_id" {
+  value = "${azurerm_subnet.prod_db_subnet.id}"
+}
