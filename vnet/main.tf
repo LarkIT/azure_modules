606d8fe3da43659de23273851ca7c00068026c3f
Nick Fosdick <nick@lark-it.com>
2018-05-06 21:44:18 -0600

    [THEM-5] Clean up and resource_group update

--- vnet/main.tf
+++ vnet/main.tf
@@ -18,43 +18,19 @@ resource "azurerm_subnet" "test_dmz_subnet" {
     name                 = "${var.environment}_${var.application_name}_dmz_subnet"
     resource_group_name  = "${local.resource_group}"
     virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-    address_prefix       = "${var.network[ "test_dmz_subnet" ]}"
+    address_prefix       = "${var.network[ "test_dmz" ]}"
 }
 
 resource "azurerm_subnet" "test_app_subnet" {
     name                 = "${var.environment}_${var.application_name}_app_subnet"
     resource_group_name  = "${local.resource_group}"
     virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-    address_prefix       = "${var.network[ "test_app_subnet" ]}"
+    address_prefix       = "${var.network[ "test_app" ]}"
 }
 
 resource "azurerm_subnet" "test_db_subnet" {
     name                 = "${var.environment}_${var.application_name}_db_subnet"
     resource_group_name  = "${local.resource_group}"
     virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-    address_prefix       = "${var.network[ "test_db_subnet" ]}"
+    address_prefix       = "${var.network[ "test_db" ]}"
 }
-
-# Create Network Security Group and rule
-#resource "azurerm_network_security_group" "ssh" {
-#    name                = "myNetworkSecurityGroup"
-#    location            = "${var.location}"
-#    resource_group_name = "${local.resource_group}"
-#
-#    security_rule {
-#        name                       = "SSH"
-#        priority                   = 1001
-#        direction                  = "Inbound"
-#        access                     = "Allow"
-#        protocol                   = "Tcp"
-#        source_port_range          = "*"
-#        destination_port_range     = "22"
-#        source_address_prefix      = "*"
-#        destination_address_prefix = "*"
-#    }
-#
-#    tags {
-#        environment = "Terraform Demo"
-#    }
-#}
-
