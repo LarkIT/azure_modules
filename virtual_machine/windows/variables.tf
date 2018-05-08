e790ff594d24aef27eb0c6b248d8e9090aa3866d
Costa Arigirs <costa.argiris@lark-it.com>
2018-05-08 17:11:19 -0600

    Added windows and linux specific virtual machine sub-modules
    THEM-12

---
+++ virtual_machine/windows/variables.tf
@@ -0,0 +1,11 @@
+variable "rg" {}
+variable "location" {}
+variable "environment" {}
+variable "application_name" {}
+variable "subnet_id" {}
+variable "hostname" {}
+variable "admin_password" {}
+
+variable "number_servers" {
+  default = "1"
+}
