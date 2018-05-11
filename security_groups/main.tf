bf62331b0213dd383d155693ad06334f16fae47f
Nick Fosdick <nick@lark-it.com>
2018-05-10 21:33:37 -0600

    fixing priority

--- security_groups/main.tf
+++ security_groups/main.tf
@@ -18,19 +18,19 @@ resource "azurerm_network_security_rule" "winrm" {
   network_security_group_name = "${azurerm_network_security_group.windows.name}"
 }
 
-resource "azurerm_network_security_rule" "ssl" {
-  name                        = "ssl"
-  priority                    = 101
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = "${var.resource_group}"
-  network_security_group_name = "${azurerm_network_security_group.windows.name}"
-}
+#resource "azurerm_network_security_rule" "ssl" {
+#  name                        = "ssl"
+#  priority                    = 101
+#  direction                   = "Outbound"
+#  access                      = "Allow"
+#  protocol                    = "Tcp"
+#  source_port_range           = "*"
+#  destination_port_range      = "443"
+#  source_address_prefix       = "*"
+#  destination_address_prefix  = "*"
+#  resource_group_name         = "${var.resource_group}"
+#  network_security_group_name = "${azurerm_network_security_group.windows.name}"
+#}
 
 resource "azurerm_network_security_rule" "ftp" {
   name                        = "ftp"
