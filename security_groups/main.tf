2977b4e395d678738ab93c03b04e2f655d009820
Nick Fosdick <nick@lark-it.com>
2018-05-10 20:45:08 -0600

    Updating the name

--- security_groups/main.tf
+++ security_groups/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_security_group" "windows" {
-  name                = "myNetworkSecurityGroup"
+  name                = "windows"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
 }
