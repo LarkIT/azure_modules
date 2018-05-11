d8e4a723e89a6a611b0134e8c26cec9838c9cc3a
Nick Fosdick <nick@lark-it.com>
2018-05-10 18:35:43 -0600

    updating name

--- vmss/main.tf
+++ vmss/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_public_ip" "public_ip" {
 }
 
 resource "azurerm_lb" "loadbalancer" {
-  name                = "test"
+  name                = "${var.application_name}_loadbalancer"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
 
