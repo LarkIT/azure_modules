d072791af8150fd720d08283a71056d725a20717
Nick Fosdick <nick@lark-it.com>
2018-05-10 18:29:17 -0600

    UPdating variable name

--- vmss/main.tf
+++ vmss/main.tf
@@ -1,9 +1,9 @@
 resource "azurerm_public_ip" "lb_ip" {
   name                         = "test"
   location                     = "${var.location}"
-  resource_group_name          = "${var.rg}"
+  resource_group_name          = "${var.resource_group}"
   public_ip_address_allocation = "static"
-  domain_name_label            = "${var.rg}"
+  domain_name_label            = "${var.resource_group}"
 
   tags {
     environment = "staging"
