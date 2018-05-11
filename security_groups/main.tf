0c83e44476a398dca1a327cb76fb96717e1bb16b
Nick Fosdick <nick@lark-it.com>
2018-05-10 21:21:31 -0600

    security rule updates

--- security_groups/main.tf
+++ security_groups/main.tf
@@ -4,18 +4,6 @@ resource "azurerm_network_security_group" "windows" {
   resource_group_name = "${var.resource_group}"
 }
 
-#resource "azurerm_network_security_group" "ssl" {
-#  name                = "myNetworkSecurityGroup"
-#  location            = "${var.location}"
-#}
-
-#resource "azurerm_network_security_group" "ftp" {
-#  name                = "myNetworkSecurityGroup"
-#  location            = "${var.location}"
-#  resource_group_name = "${var.resource_group}"
-#}
-
-
 resource "azurerm_network_security_rule" "winrm" {
   name                        = "winrm"
   priority                    = 100
@@ -24,6 +12,34 @@ resource "azurerm_network_security_rule" "winrm" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "5986"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${var.resource_group}"
+  network_security_group_name = "${azurerm_network_security_group.windows.name}"
+}
+
+resource "azurerm_network_security_rule" "ssl" {
+  name                        = "winrm"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${var.resource_group}"
+  network_security_group_name = "${azurerm_network_security_group.windows.name}"
+}
+
+resource "azurerm_network_security_rule" "ssl" {
+  name                        = "winrm"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "21"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = "${var.resource_group}"
