203f94894c4fd0992f9c2d8818edafaa90125287
Nick Fosdick <nick@lark-it.com>
2018-05-10 21:26:12 -0600

    minor

--- virtual_machine/linux/main.tf
+++ virtual_machine/linux/main.tf
@@ -1,25 +1,25 @@
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "ssh" {
-  name                = "myNetworkSecurityGroup"
-  location            = "${var.location}"
-  resource_group_name = "${var.rg}"
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  tags {
-    environment = "Terraform Demo"
-  }
-}
+#resource "azurerm_network_security_group" "ssh" {
+#  name                = "myNetworkSecurityGroup"
+#  location            = "${var.location}"
+#  resource_group_name = "${var.rg}"
+#
+#  security_rule {
+#    name                       = "SSH"
+#    priority                   = 1001
+#    direction                  = "Inbound"
+#    access                     = "Allow"
+#    protocol                   = "Tcp"
+#    source_port_range          = "*"
+#    destination_port_range     = "22"
+#    source_address_prefix      = "*"
+#    destination_address_prefix = "*"
+#  }
+#
+#  tags {
+#    environment = "Terraform Demo"
+#  }
+#}
 
 resource "azurerm_public_ip" "public_ip" {
   count                        = "${var.number_servers}"
